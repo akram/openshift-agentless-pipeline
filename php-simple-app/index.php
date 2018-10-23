16942fd1eb23e463c2e682b47f3d519101cc1e17
Akram Ben Aissi <akram.benaissi@gmail.com>
2018-10-23 20:22:44 +0200

    fix

--- php-simple-app/index.php
+++ php-simple-app/index.php
@@ -3,7 +3,7 @@
     <title>PHP Simple App</title>
   </head>
   <body>
-    <? $date = new DateTime('2000-01-01');
+    <?php $date = new DateTime('2000-01-01');
        echo "Hello ". $date->format('Y-m-d H:i:s') ?>
  </body>
 </html>
