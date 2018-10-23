f39dfb116c4ad6a98ad5ec6eeb41a4ef3569c8a7
Akram Ben Aissi <akram.benaissi@gmail.com>
2018-10-23 20:27:29 +0200

    fix

--- php-simple-app/index.php
+++ php-simple-app/index.php
@@ -3,8 +3,29 @@
     <title>PHP Simple App</title>
   </head>
   <body>
-    <?php $date = new DateTime('2000-01-01');
+    <?php $date = new DateTime();
        echo "Hello ". $date->format('Y-m-d H:i:s') ?>
+
+    <h1>Titre principal</h1>
+    <h2>Sous titre</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Duis eget ligula quis libero mollis dapibus.
+       Suspendisse potenti. Nullam facilisis neque et sem. 
+       Proin placerat adipiscing urna. Aenean sollicitudin. 
+       Mauris lorem erat, fringilla quis, sagittis a, varius sed, nunc. 
+       Pellentesque ligula. Nullam egestas eleifend turpis. Vivamus ac
+       sapien. Sed venenatis, ligula ut scelerisque vehicula, 
+       erat tellus euismod ipsum, eget faucibus tortor arcu et lectus.
+       Vivamus vel purus. Fusce dignissim tortor quis diam elementum fermentum. 
+       Mauris eleifend lorem vel arcu. Vivamus tempus
+       faucibus lectus. Curabitur volutpat ornare mi. 
+       Curabitur ac libero. Sed eu elit ac metus egestas iaculis.
+    </p>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
  </body>
 </html>
  
